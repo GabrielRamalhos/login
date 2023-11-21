ef9313c8c1a698b5f89237eb9a5bf6f10867e9aa
unknown <gabriel.ramalhos96@gmail.com>
2023-11-21 20:23:22 -0300

    1 formulario

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert('Valide seus gastos')
+const btnSomar = document.querySelector('#btnSomar')
+
+btnSomar.addEventListener('click', () => {
+    soma()
+})
+
+function soma() {
+   const campovalor1 = document.querySelector('#valor1')
+   const campovalor2 = document.querySelector('#valor2')
+   const valor1 = parseInt(campovalor1.value)
+   const valor2 = parseInt(campovalor2.value)
+   const valorSomado = valor1 + valor2
+   const campoResultado = document.querySelector('#resultado');
+   campoResultado.textContent = valorSomado
+}
+    
