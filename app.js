5ccea656d25eed8084d345a4b2bcce085b73e40b
unknown <gabriel.ramalhos96@gmail.com>
2023-11-21 20:27:37 -0300

    1 formulario

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ btnSomar.addEventListener('click', () => {
 function soma() {
    const campovalor1 = document.querySelector('#valor1')
    const campovalor2 = document.querySelector('#valor2')
+   const campovalor3 = document.querySelector('#valor2')
+   const campovalor4 = document.querySelector('#valor2')
+
    const valor1 = parseInt(campovalor1.value)
    const valor2 = parseInt(campovalor2.value)
-   const valorSomado = valor1 + valor2
+   const valor3 = parseInt(campovalor3.value)
+   const valor4 = parseInt(campovalor4.value)
+   const valorSomado = valor1 + valor2 + valor3 + valor4
    const campoResultado = document.querySelector('#resultado');
    campoResultado.textContent = valorSomado
 }
